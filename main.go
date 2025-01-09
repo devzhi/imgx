22f69a4f7d2d4dd38ee12f4a2068dbb6ad93b030
devzhi <devzhi@outlook.com>
2025-01-09 17:49:47 +0800

    连接pull所有模块

--- main.go
+++ main.go
@@ -3,19 +3,50 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/devzhi/imgx/internal/pull"
 )
 
 var (
 	imageName = flag.String("name", "", "name of the image")
 	tag       = flag.String("tag", "latest", "tag of the image")
 	arch      = flag.String("arch", "amd64", "architecture of the image")
+	os        = flag.String("os", "linux", "operating system of the image")
 )
 
 func main() {
+	// 解析参数
 	flag.Parse()
 	if *imageName == "" {
 		fmt.Println("Image name is required")
 		return
 	}
 	fmt.Println("Pulling image", *imageName, "with tag", *tag, "for architecture", *arch)
+	// 获取token
+	token, err := pull.GetToken(*imageName)
+	if err != nil {
+		fmt.Println("Error getting token", err)
+		return
+	}
+	// 获取镜像清单
+	manifest, err := pull.GetManifest(token, *imageName, *tag, *arch, *os)
+	if err != nil {
+		fmt.Println("Error getting manifest", err)
+		return
+	}
+	// 下载镜像
+	path, err := pull.DownloadImage(token, manifest, *arch, *os, *imageName, *tag)
+	if err != nil {
+		fmt.Println("Error downloading image", err)
+		return
+	}
+	fmt.Println("Image downloaded to", *path)
+	// 删除临时文件
+	defer pull.RemoveImageSaveDir(*imageName, *tag, *arch, *os)
+	// 打包镜像
+	outputFile, err := pull.Package(*path, *imageName, *tag, *arch, *os, nil)
+	if err != nil {
+		fmt.Println("Error packaging image", err)
+		return
+	}
+	fmt.Println("Image packaged to", *outputFile)
 }
