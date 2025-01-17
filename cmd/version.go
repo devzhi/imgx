2f8fcc62b60d1093a883be30a3587d45a42a1c85
devzhi <devzhi@outlook.com>
2025-01-17 17:10:57 +0800

    docs: 修复大小写错误

--- cmd/version.go
+++ cmd/version.go
@@ -7,7 +7,7 @@ import (
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "show imgx version info.",
+	Short: "Show imgx version info.",
 	Run: func(cmd *cobra.Command, args []string) {
 		version := "v0.1.0"
 		fmt.Println("imgx version", version)
