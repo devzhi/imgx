a3e720d2f687508c67b8222539fa7e82dc4fc8ee
devzhi <devzhi@outlook.com>
2025-01-18 12:38:57 +0800

    fix: rename

--- internal/pull/handler.go
+++ internal/pull/handler.go
@@ -13,24 +13,24 @@ type Flag struct {
 }
 
 // Execute 执行pull命令
-func Execute(command *Flag) (*string, error) {
+func Execute(flag *Flag) (*string, error) {
 	// 执行pull命令
 	// 获取token
-	token, err := GetToken(command.Image)
+	token, err := GetToken(flag.Image)
 	if err != nil {
 		fmt.Println("Error getting token", err)
 		return nil, err
 	}
 
 	// 获取镜像清单
-	manifest, err := GetManifest(token, command.Image, command.Tag, command.Arch, command.OsFlag)
+	manifest, err := GetManifest(token, flag.Image, flag.Tag, flag.Arch, flag.OsFlag)
 	if err != nil {
 		fmt.Println("Error getting manifest", err)
 		return nil, err
 	}
 
 	// 下载镜像
-	path, err := DownloadImage(token, manifest, command.Arch, command.OsFlag, command.Image, command.Tag)
+	path, err := DownloadImage(token, manifest, flag.Arch, flag.OsFlag, flag.Image, flag.Tag)
 	if err != nil {
 		fmt.Println("Error downloading image", err)
 		return nil, err
@@ -38,10 +38,10 @@ func Execute(command *Flag) (*string, error) {
 	fmt.Println("Image downloaded to", *path)
 
 	// 删除临时文件
-	defer RemoveImageSaveDir(command.Image, command.Tag, command.Arch, command.OsFlag)
+	defer RemoveImageSaveDir(flag.Image, flag.Tag, flag.Arch, flag.OsFlag)
 
 	// 打包镜像
-	outputFile, err := Package(*path, command.Image, command.Tag, command.Arch, command.OsFlag, nil)
+	outputFile, err := Package(*path, flag.Image, flag.Tag, flag.Arch, flag.OsFlag, nil)
 	if err != nil {
 		fmt.Println("Error packaging image", err)
 		return nil, err
